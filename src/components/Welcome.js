affb44f025699108a85c8af10023418b517d73ae
Gustavo <gustavo.guarnieri@gmail.com>
2021-03-13 16:08:04 -0300

    fixed className tag

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,9 +2,9 @@ import UserService from "../services/UserService";
 
 const Welcome = () => (
    <div className="jumbotron">
-       <div class="mx-auto text-center">
-           <h1 class="mx-auto my-0 text-uppercase">Buy food</h1>
-           <h2 class="text-black-50 mx-auto mt-2 mb-5">O lugar certo para matar sua fome!</h2>
+       <div className="mx-auto text-center">
+           <h1 className="mx-auto my-0 text-uppercase">Buy food</h1>
+           <h2 className="text-black-50 mx-auto mt-2 mb-5">O lugar certo para matar sua fome!</h2>
            <button className="btn btn-lg btn-warning" onClick={() => UserService.doLogin()}>Login</button>
        </div>
    </div>
