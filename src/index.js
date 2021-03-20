7f5364a3bdf8296d86203e1d943bcec2594eaeca
Gustavo <gustavo.guarnieri@gmail.com>
2021-03-20 14:17:22 -0300

    identified code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./components/App";
 import HttpService from "./services/HttpService";
 import UserService from "./services/UserService";
 
+import "bootstrap/dist/js/bootstrap.min.js"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
 
