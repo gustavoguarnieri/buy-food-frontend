1bf40f612d2f47846e3ab3af02259410752ac6b3
Gustavo <gustavo.guarnieri@gmail.com>
2021-03-30 00:32:57 -0300

    added payment and preparation components utils

---
+++ src/components/Utils/PaymentStatus.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {Form} from "react-bootstrap";
+
+function PaymentStatus(props) {
+
+    return (
+        <>
+            <Form.Control
+                value={props.paymentStatus}
+                onChange={props.handlePaymentStatusChange}
+                as="select"
+                className="mr-sm-0"
+                id="inlineFormCustomSelect"
+                readOnly={props.isReadOnly}
+            >
+                {props.isSelectVisible ?
+                    <option
+                        key="-1"
+                        value="-1">Selecione...
+                    </option> : <></>
+                }
+
+                <option
+                    key="APPROVED"
+                    value="APPROVED"
+                >
+                    APROVADO
+                </option>
+                <option
+                    key="PENDING"
+                    value="PENDING"
+                >
+                    PENDENTE
+                </option>
+                <option
+                    key="DECLINED"
+                    value="DECLINED"
+                >
+                    RECUSADO
+                </option>
+
+            </Form.Control>
+        </>
+    )
+}
+
+export default PaymentStatus;
---
+++ src/components/Utils/PreparationStatus.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {Form} from "react-bootstrap";
+
+function PreparationStatus(props) {
+
+    return (
+        <>
+            <Form.Control
+                value={props.preparationStatus}
+                onChange={props.handlePreparationStatusChange}
+                as="select"
+                className="mr-sm-0"
+                id="inlineFormCustomSelect"
+                readOnly={props.isReadOnly}
+            >
+                {props.isSelectVisible ?
+                    <option
+                        key="-1"
+                        value="-1">Selecione...
+                    </option> : <></>
+                }
+
+                <option
+                    key="PENDING"
+                    value="PENDING"
+                >
+                    PENDENTE
+                </option>
+                <option
+                    key="COOKING"
+                    value="COOKING"
+                >
+                    COZINHANDO
+                </option>
+                <option
+                    key="DELIVERY"
+                    value="DELIVERY"
+                >
+                    ENTREGANDO
+                </option>
+
+            </Form.Control>
+        </>
+    )
+}
+
+export default PreparationStatus;
