ce59800afd7fd7873949e07baae7f92b4346d7a2
Gustavo <gustavo.guarnieri@gmail.com>
2021-04-04 16:49:27 -0300

    improved buy food welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,26 @@
 import UserService from "../services/UserService";
+import {Col, Figure, Row} from "react-bootstrap";
+import React from "react";
 
 const Welcome = () => (
-   <div className="jumbotron">
-       <div className="mx-auto text-center">
-           <h1 className="mx-auto my-0 text-uppercase">Buy food</h1>
-           <h2 className="text-black-50 mx-auto mt-2 mb-5">O lugar certo para matar sua fome!</h2>
-           <button className="btn btn-lg btn-warning" onClick={() => UserService.doLogin()}>Login</button>
-       </div>
-   </div>
+    <>
+        <Row>
+            <Col md="12">
+                <div className="buy-food-jumbotron">
+                    <div className="text-center">
+                        <h1 className="mx-auto my-0 buy-food-title">Buy food</h1>
+                        <h2 className="text-black-50 mx-auto mt-2 mb-5 buy-food-subtitle">O lugar certo para matar sua fome!</h2>
+                        <button className="btn btn-lg btn-danger" onClick={() => UserService.doLogin()}>Login</button>
+                    </div>
+                </div>
+            </Col>
+        </Row>
+        <Row>
+            <Col md="12">
+                <div className="buy-food-title-img"/>
+            </Col>
+        </Row>
+    </>
 )
 
 export default Welcome
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import UserService from "./services/UserService";
 
+import './assets/fonts/text/Coiny-Regular.ttf';
+import './assets/fonts/text/Livvic-Light.ttf';
 import "bootstrap/dist/js/bootstrap.min.js"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
