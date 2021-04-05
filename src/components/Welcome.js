84d197f2fd6f2a9253a914bdfb94632eaecdea7a
Gustavo <gustavo.guarnieri@gmail.com>
2021-04-04 22:17:24 -0300

    improved welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,25 +1,21 @@
 import UserService from "../services/UserService";
-import {Col, Figure, Row} from "react-bootstrap";
+import {Col} from "react-bootstrap";
 import React from "react";
 
 const Welcome = () => (
     <>
-        <Row>
-            <Col md="12">
-                <div className="buy-food-jumbotron">
-                    <div className="text-center">
-                        <h1 className="mx-auto my-0 buy-food-title">Buy food</h1>
-                        <h2 className="text-black-50 mx-auto mt-2 mb-5 buy-food-subtitle">O lugar certo para matar sua fome!</h2>
-                        <button className="btn btn-lg btn-danger" onClick={() => UserService.doLogin()}>Login</button>
-                    </div>
+        <Col md="12">
+            <div className="buy-food-jumbotron">
+                <div className="text-center">
+                    <h1 className="mx-auto my-0 buy-food-title">Buy food</h1>
+                    <h2 className="text-black-50 mx-auto mt-2 mb-5 buy-food-subtitle">O lugar certo para matar sua fome!</h2>
+                    <button className="btn btn-lg btn-danger" onClick={() => UserService.doLogin()}>Login</button>
                 </div>
-            </Col>
-        </Row>
-        <Row>
-            <Col md="12">
-                <div className="buy-food-title-img"/>
-            </Col>
-        </Row>
+            </div>
+        </Col>
+        <Col md="12">
+            <div className="buy-food-title-img"/>
+        </Col>
     </>
 )
 
